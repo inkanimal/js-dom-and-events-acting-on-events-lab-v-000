4f84d38e72d7c3923cd2387b842c1755779aed71
geoffrey woodcock <theatomproject@gmail.com>
2019-06-02 20:59:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,15 @@ function addNewElementAsLi(){
 }
 
 function addNewLiOnClick(){
-  
+  const employee_info = retrieveEmployeeInformation()
+  submitButton = document.querySelector('body main input[type = submit]')
+  submitButton.addEventListener('click', function(event) {
+    const new_li = document.createElement('li')
+    new_li.innerHTML = employee_info
+    document.querySelector('ul').appendChild(new_li)
+    const input = document.querySelector('main form input')
+    input.value = ""
+  });
 }
 
 function clearEmployeeListOnLinkClick(){
